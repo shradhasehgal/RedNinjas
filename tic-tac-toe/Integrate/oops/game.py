073cd8cfd45f4f65ff0bf0e70ecf2560075cab1f
Mallika <mallika2011@yahoo.com>
2020-07-08 23:49:46 +0530

    Create separate folder for integration
    - Include new functions in Game
    - Create function for agent_next_move in Game
    - Create clear_game function

---
+++ tic-tac-toe/Integrate/oops/game.py
@@ -0,0 +1,122 @@
+import math
+import numpy as np
+import random
+from constants import *
+
+from board import *
+from minimax import *
+from move import *
+from player import *
+from score import *
+from bigtree import *
+
+class Game ():
+
+    def __init__(self):
+
+        self.board_obj = Board()
+        self.human_obj = Human()
+        self.agent_obj = Agent()
+        self.bigtree_obj = Bigtree()
+        self.minimax_obj = Minimax()
+
+    def initialize_bigtree(self,is_max):
+
+        # Calling the minmax function for the entire tree : if depth_type == -1
+        self.minimax_obj.minimax(self.board_obj, 0, is_max,self.bigtree_obj)
+
+
+    def check_win(self):
+        for i in range(0, 3):
+            if(self.board_obj.board[i][0] == self.board_obj.board[i][1] == self.board_obj.board[i][2] != EMPTY):
+                return True
+
+        for j in range(0, 3):
+            if(self.board_obj.board[0][j] == self.board_obj.board[1][j] == self.board_obj.board[2][j] != EMPTY):
+                return True
+
+        if self.board_obj.board[0][0] == self.board_obj.board[1][1] == self.board_obj.board[2][2] != EMPTY:  # diagonal
+            return True
+
+        if self.board_obj.board[0][2] == self.board_obj.board[1][1] == self.board_obj.board[2][0] != EMPTY:  # diagonal
+            return True
+
+        return False
+
+    def clear_game(self):
+        self.board_obj.clear_board()
+    
+
+    def agent_next_move(self, board, depth_type):
+        self.board_obj.board = board
+        print("self == ", self.board_obj.board)
+        r,c = self.agent_obj.agent_turn(self.board_obj,self.minimax_obj,self.bigtree_obj,depth_type)
+        win = self.check_win()
+
+        print(r,c,win)
+
+        return r,c,win
+
+
+    def game(self):
+    
+        depth_type = int(input("Enter A Depth Type : "))
+        self.board_obj.clear_board()
+        turn = AGENT
+        count = 0
+        '''Considering agent always as a maximiser
+            if HUMAN should play first ; turn=HUMAN , is_max=False
+            if AGENT should play first ; turn=AGENT , is_max=True '''
+        
+
+        while(True):
+
+            if(turn == HUMAN):
+                self.human_obj.human_turn(self.board_obj)
+                count += 1
+            else:
+                self.agent_obj.agent_turn(self.board_obj,self.minimax_obj,self.bigtree_obj,depth_type)
+                count += 1
+
+            did_win = False
+            # Now we will check if player X or O has won,for every move after 5 moves.
+            if count >= 5:
+
+                did_win = self.check_win()
+
+            if did_win:
+                break
+
+            # If neither X nor O wins and the board is full, we'll declare the result as 'tie'.
+            if count == 9:
+                print("\nGame Over.\n")
+                print("It's a Tie!!")
+
+            # Now we have to change the player after every move.
+            if turn == HUMAN:
+                turn = AGENT
+            else:
+                turn = HUMAN
+
+        # Now we will ask if player wants to restart the game or not.
+        restart = input("Do want to play Again?(y/n)")
+        if restart == "y" or restart == "Y":
+            self.game()
+
+
+if __name__ == '__main__':
+    
+    g = Game()
+
+    #is agent maximizer
+    is_max = True
+
+    #Inialise bigtree for all subsequent games
+    g.initialize_bigtree(is_max)
+
+    #start playing
+    # g.game()
+
+    a = np.array(([['O','O',EMPTY],['X','X',EMPTY],[EMPTY,EMPTY,EMPTY]]),dtype=str)
+
+    g.agent_next_move(a,-1)
---
+++ tic-tac-toe/Integrate/oops/move.py
@@ -0,0 +1,78 @@
+import math
+import numpy as np
+import random
+from constants import *
+
+
+class Move ():
+
+    def choose_optimal_move(self,board_obj, depth_type, minimax_obj, bigtree_obj):
+    
+        optimal_val = -math.inf
+
+        # From no.of empty squares we can find the value of depth
+        depth = 9
+
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if(board_obj.board[i][j] == EMPTY):
+                    depth = depth-1
+
+        # In case of ultimate : proceed as usual
+        if(depth_type == -1):
+            
+            current_States = bigtree_obj.bigtree[depth]
+
+            for i in range(len(current_States)):
+                comparison = current_States[i][0] == board_obj.board
+                equal_arrays = comparison.all()
+
+                if equal_arrays:
+                        best_val_row = current_States[i][1]
+                        best_val_col = current_States[i][2]
+
+        # In case of depth based : use customized minmax
+        else:
+            best_val_row, best_val_col, best_val = self.compute_next_move_depths(
+                board_obj, depth_type, minimax_obj)
+
+        return best_val_row, best_val_col
+
+
+    def compute_next_move_depths(self, board_obj, depth_type, minimax_obj):
+        
+        optimal_val = -math.inf
+        random_cell = []
+
+        for i in range(0, 3):
+            for j in range(0, 3):
+
+                if board_obj.board[i][j] == EMPTY:
+
+                    # Iterate over all the empty cells to pick one
+                    if depth_type == 1:
+                        random_cell.append((i, j))
+
+                    else:
+                        # Player move?
+                        board_obj.board[i][j] = AGENT
+
+                        # Agent is maximizer
+                        move_val = minimax_obj.run_custom_minimax(board_obj, 0, depth_type, i, j, False)
+
+                        # Player unmove?
+                        board_obj.board[i][j] = EMPTY
+                        if move_val > optimal_val:
+
+                            best_val_row = i
+                            best_val_col = j
+                            optimal_val = move_val
+
+        # Randomly picking a cell to fill in
+        if(depth_type == 1):
+            cell = random.choice(random_cell)
+            best_val_row = cell[0]
+            best_val_col = cell[1]
+
+        print("for the agents move, returning ", best_val_row,best_val_col)
+        return best_val_row, best_val_col, optimal_val
