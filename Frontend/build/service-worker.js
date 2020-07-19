c6f1efd41a64a81d0403e803a1744fbb264d4ae3
Shradha <shradha.sgl@gmail.com>
2020-07-20 00:30:33 +0530

    Added 404 page

--- Frontend/build/service-worker.js
+++ Frontend/build/service-worker.js
@@ -14,7 +14,11 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
+<<<<<<< HEAD
   "/precache-manifest.e4e2cb24a4448d1da9ec20328d8003fb.js"
+=======
+  "/precache-manifest.a2b5c03f2b52aba2850982f2f87b241f.js"
+>>>>>>> d0410896... Added 404 page
 );
 
 self.addEventListener('message', (event) => {
